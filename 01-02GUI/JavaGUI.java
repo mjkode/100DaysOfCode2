4d2d8d99bcc32f64733fdcd0a942a330a800b758
Michael Kiger <mjk@wpsitework.com>
2021-09-30 13:46:58 -0400

    Listeners

--- 01-02GUI/JavaGUI.java
+++ 01-02GUI/JavaGUI.java
@@ -29,10 +29,10 @@ public class JavaGUI {
         JavaGUI window = new JavaGUI();
 
 		// Set label size/position.
-		window.label.setBounds(50,20,100,30);
+		window.label.setBounds(50,50,100,30);
 
 		// Set button size/position.	
-		window.button.setBounds(50,60,100,30);    
+		window.button.setBounds(50,100,100,30);    
 
 		// Set window size.
         window.frame.setSize(400, 400);
@@ -40,11 +40,24 @@ public class JavaGUI {
         // Set window layout.
         window.frame.setLayout(null);
 
+		// Set window position to center of screen
+		window.frame.setLocationRelativeTo(null);
+
+        // Set window Color
+        window.frame.setBackground(Color.BLUE);
+
         // Add label to frame so button can be seen.
         window.frame.add(window.label);
-		
+
+        // Set label Color
+        window.label.setBackground(Color.GREEN);
+
        // Add button to frame so button can be seen.
 		window.frame.add(window.button);
+
+        // Set label Color
+        window.button.setBackground(Color.RED);
+
  
         // Register window listener event to frame so window can be closed.
         window.frame.addWindowListener(
---
+++ 03-04GUI/SwingGUI.java
@@ -0,0 +1,61 @@
+// Import Swing componets
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class SwingGUI{
+	
+	// Component method: Creat, set, add to panel
+	private static void placeComponents(JPanel panel) {
+		panel.setLayout(null);
+
+		JLabel userLabel = new JLabel("User");
+		userLabel.setBounds(10, 10, 80, 25);
+		panel.add(userLabel);
+
+		JTextField userText = new JTextField("Enter Name", 20);
+		userText.setBounds(100, 10, 160, 25);
+		panel.add(userText);
+
+		JLabel passwordLabel = new JLabel("Password");
+		passwordLabel.setBounds(10, 40, 80, 25);
+		panel.add(passwordLabel);
+
+		JPasswordField passwordText = new JPasswordField(20);
+		passwordText.setBounds(100, 40, 160, 25);
+		passwordText.setEchoChar((char)0);
+		passwordText.setText("enter password");
+		panel.add(passwordText);
+
+		JButton loginButton = new JButton("login");
+		loginButton.setBounds(10, 80, 80, 25);
+		panel.add(loginButton);
+		
+		JButton registerButton = new JButton("register");
+		registerButton.setBounds(180, 80, 80, 25);
+		panel.add(registerButton);
+	}
+	
+
+	// Main method: Entry point
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Demo application"); // Create
+		frame.setSize(300, 150); // Size
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close
+
+		JPanel panel = new JPanel(); // Create
+		frame.add(panel); //Add panel to frame
+		placeComponents(panel); // Run Add componets to panel
+		frame.setLocationRelativeTo(null); // Set window position to center of screen
+		frame.setVisible(true);
+	}
+
+
+}
