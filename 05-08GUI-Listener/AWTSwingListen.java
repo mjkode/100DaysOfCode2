29f04cf3a0960da5a60cd6f4d97e8d0f2b61fcc5
Michael Kiger <mjk@wpsitework.com>
2021-10-05 23:12:24 -0400

    Listeners

--- 05-08GUI-Listener/AWTSwingListen.java
+++ 05-08GUI-Listener/AWTSwingListen.java
@@ -1,51 +1,65 @@
+// Import Libraries
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class AWTSwingListen
-{
-  static JFrame frame;
 
-  public static void main(String[] args)
-  {
-    // schedule this for the event dispatch thread (edt)
-    SwingUtilities.invokeLater(new Runnable()
-    {
-      public void run()
-      {
-        displayJFrame();
-      }
-    });
-  }
+public class AWTSwingListen{
+	static JFrame frame;
 
-  static void displayJFrame()
-  {
-    frame = new JFrame("Our JButton listener example");
+	public static void main(String[] args){
+		// schedule this for the event dispatch thread (edt)
+		//SwingUtilities.invokeLater(new Runnable(){
+		//	public void run(){
+				displayJFrame();
+			//}
+		//});
+	}
 
-    // create our jbutton
-    JButton showDialogButton = new JButton("Click Me");
+	static void displayJFrame(){
+		frame = new JFrame("Our JButton listener example");
+
+		
+		// create texbox
+		JTextField username = new JTextField("Username");
+		JTextField password = new JTextField("Password");
+		
+		//Create the combo box, select item at index
+		String[] divStrings = { "Cmd", "Eng", "Sec", "Med", "Com" };
+		JComboBox divList = new JComboBox(divStrings);
+		divList.setSelectedIndex(4);
+		divList.addActionListener(new ActionListener(){});
+		
+		// create jbutton
+		JButton showDialogButton = new JButton("Login");
     
-    // add the listener to the jbutton to handle the "pressed" event
-    showDialogButton.addActionListener(new ActionListener()
-    {
-      public void actionPerformed(ActionEvent e)
-      {
-        // display/center the jdialog when the button is pressed
-        JDialog d = new JDialog(frame, "Hello", true);
-        d.setLocationRelativeTo(frame);
-        d.setVisible(true);
-      }
-    });
+		// add the listener to the jbutton to handle the "pressed" event
+		showDialogButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				// display/center the jdialog when the button is pressed
+								
+				if(password.getText() == "bob"){
+					JDialog dialog = new JDialog(frame, password.getText(), true);
+				}
+				else{
+					JDialog dialog = new JDialog(frame, "is not the password", true);
+				};	
+				//JDialog dialog = new JDialog(frame, password.getText(), true);
+				frame.dialog.setLocationRelativeTo(frame);
+				frame.dialog.setVisible(true);
+			}
+		});
 
-    // put the button on the frame
-    frame.getContentPane().setLayout(new FlowLayout());
-    frame.add(showDialogButton);
-
-    // set up the jframe, then display it
-    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    frame.setPreferredSize(new Dimension(300, 200));
-    frame.pack();
-    frame.setLocationRelativeTo(null);
-    frame.setVisible(true);
-  }
+		// put the button on the frame
+		frame.getContentPane().setLayout(new FlowLayout());
+		frame.add(password);
+		frame.add(showDialogButton);
+		frame.add(divList);
+		// set up the jframe, then display it
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setPreferredSize(new Dimension(300, 200));
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
 }
---
+++ 09-14GUI-MultiListener/LoginButtonListener.java
@@ -0,0 +1,10 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+
+public class LoginButtonListener implements ActionListener {
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JOptionPane.showMessageDialog(null, "login button has been pressed");
+	}
+}
